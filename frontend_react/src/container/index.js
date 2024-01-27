976370bdca5df5af17899d58a94d2403c96419d8
Faraz Chand <faraz.chand@hotmail.com>
2024-01-26 19:30:45 -0500

    general update 1/26/2024

--- frontend_react/src/container/index.js
+++ frontend_react/src/container/index.js
@@ -1,7 +1,5 @@
-import Testimonial from "./Testimonial/Testimonial.jsx";
-
-export { default as About } from "./About/About.jsx";
 export { default as Footer } from "./Footer/Footer.jsx";
+export { default as About } from "./About/About.jsx";
 export { default as Header } from "./Header/Header.jsx";
 export { default as Skills } from "./Skills/Skills.jsx";
 export { default as Testimonial } from "./Testimonial/Testimonial.jsx";
